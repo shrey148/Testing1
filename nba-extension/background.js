8350a9d993b575fec21e72e2b044bf1fbf51c0c2
Shrey Tripathi <155367185+shrey148@users.noreply.github.com>
2025-06-28 19:52:34 +0530

    Fix alarm access in NBA extension service worker

---
+++ nba-extension/background.js
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  if (chrome.alarms && chrome.alarms.create) {
+    chrome.alarms.create('updateNBA', { periodInMinutes: 10 });
+  }
+});
+
+if (chrome.alarms && chrome.alarms.onAlarm) {
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'updateNBA') {
+      chrome.action.setBadgeText({ text: '...' });
+      fetch('https://www.balldontlie.io/api/v1/games?per_page=1')
+        .then(resp => resp.json())
+        .then(() => {
+          chrome.action.setBadgeText({ text: '' });
+        })
+        .catch(() => {
+          chrome.action.setBadgeText({ text: '!' });
+        });
+    }
+  });
+}
